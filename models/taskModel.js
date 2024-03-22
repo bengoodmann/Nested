913b7966384f634efcdead7d7f12cfd478978229
Benson "Santi" Isaac <oluchukwubensonisaac@gmail.com>
2024-03-22 03:18:33 +0100

    Task Model

---
+++ models/taskModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: { type: String, required: true },
+  description: { type: String },
+  dueDate: { type: Date, required: true },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  completed: { type: Boolean, default: false },
+  category: {
+    name: { type: String, required: true },
+    color: { type: String },
+  },
+  reminders: [
+    {
+      reminderDate: { type: Date, required: true },
+    },
+  ],
+
+  timestamps: true,
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = { Task };
--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,15 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add your first name or full name"],
-    maxlength: 50,
-  },
-
-  email: {
-    type: String,
-    required: [true, "Please add your email"],
-    unique: [true, "Email already taken"],
-  },
-  profilePicture: {
-    type: String,
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  passwordResetToken: {
-    type: String,
-    required: false,
-  },
+  name: { type: String, required: true, maxlength: 50 },
+  email: { type: String, required: true, unique: true },
+  profilePicture: { type: String },
+  verified: { type: Boolean, default: false },
+  verificationToken: { type: String },
+  password: { type: String, required: true },
+  passwordResetToken: { type: String, required: false },
 });
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
